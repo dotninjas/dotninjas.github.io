13e175306db47cfd553391623263ed90b93e00bb
Tim Menzies <tim@menzies.us>
2016-07-01 15:11:33 +0200

    saving

--- ninja/sway.py
+++ ninja/sway.py
@@ -6,17 +6,19 @@ sys.dont_write_bytecode=True
 ABOUT=[
   "sway: Data mining+optimization",
   "(C) 2016, tim@menzies.us MIT,v2",
-  ("--samples",     256,   int, "I", "keep at most, say, 256 samples"),
-  ("--cdomBigger", 0.01, float, "F", "continous domination bigger; e.g. 1.01"),
-  ("--swayBigger", 1.01,,float, "F", "SWAY projections bigger; e.g. 1.01"),
-  ("--swayCull",    0.5,   int, "F", "SWAY's smallest population size; e.g. 0.5"),
-  ("--swayStop",     20,   int, "I", "SWAY's smallest size, leaf clusters; e.g. 20")
-]
+  ("--seed",   61409389,   int,   "int", "random number seed"),
+  ("--samples",     256,   int,   "int", "keep at most, say, 256 samples"),
+  ("--cdomBigger", 0.01, float, "float", "continous domination bigger; e.g. 1.01"),
+  ("--kind",      "Row",   str,   "str", "kind of model class for each Row"),
+  ("--swayBigger", 1.01, float, "float", "SWAY projections bigger; e.g. 1.01"),
+  ("--swayCull",    0.5,   int, "float", "SWAY's smallest population size; e.g. 0.5"),
+  ("--swayStop",     20,   int,   "int", "SWAY's smallest size, leaf clusters; e.g. 20")
+] 
 
 def cmdline(lst):
-  parser = argparse.ArgumentParser(prog= lst[0], description= lst[1])
+  parser = argparse.ArgumentParser(prog=lst[0], description= lst[1])
   for (key, d, t, m,h,) in lst[2:]:
-    parser.add_argument(key, default=d, type=t,metavar=m,help=h)
+    parser.add_argument(key,default=d,type=t,metavar=m,help=h)
   return parser.parse_args()
 
 THE = cmdline(ABOUT)
@@ -28,23 +30,48 @@ but realistically, not deliver numbers that are
 orders of magnitude different
 """
 
-UNKNOWN= "?"
-LOVE=    ">"
-HATE=    "<"
-KLASS=   "="
-SKIP=    "-"
-
-def same(x):   return x
+def same(x)  : return x
 def less(x,y): return x < y
 def more(x,y): return x > y
 def max(x,y) : return x if x>y else y
 def min(x,y) : return x if x<y else y
 def any(lst) : return random.choose(list)
+def rseed(s) : random.seed(s or THE.seed)
 
 def atom(x):
   try: return float(x),Num
   except ValueError: return x,Sym
+  
+class Row(object):
+  rid = 0
+  def __init__(i,lst):
+    i.contents=lst
+    i.rid = Row.rid = Row.rid+1
+  def __repr__(i)       : return '#%s,%s' % (i.rid,i.contents)
+  def __getitem__(i,k)  : return i.contents[k]
+  def __setitem__(i,k,v): i.contents[k] = v
 
+
+#---------    
+ 
+class Model(Row):
+  def __init__(i.lst):
+    super(Model, i).__init__(lst)
+    i.labelled=False
+  def label(i,tbl,cost=1):
+     """Row labels are cached back into the row. So labelling
+        N times only incur a single labelling cost."""
+    if not i.labelled:
+      i.label1()
+      tbl.cost += cost
+      for col in tbl.dep:
+        col.add( i[col.pos] ) # and remember the new values
+      i.labelled = True
+    return i
+  def label1(i):
+    "Rewritten by subclass"
+    return i
+  
 class Summary: pass
 
 class Num(Summary):
@@ -101,22 +128,23 @@ class Sym(Summary):
 
 class Thing(Summary):
   "some thing that can handle  Nums or Syms"
+  UNKNOWN = "?"
   def __init__(i, pos, txt):
     i.txt, i.pos, i.details, i.samples = txt, pos, None, Sample()
   def add(i,x):
-    if x != UNKNOWN:
+    if x != Thing.UNKNOWN:
       if i.details is None:
         x,what  = atom(x)
         i.details = what()
       x = i.details.add(x)
-      i.samples += x
+      i.samples.add(x)
     return x
 
 class Sample:
   def __init__(i, init=[]):
     i.n, i.some, i.ordered = 0, [], False
-    map(i.__iadd__,init)
-  def __iadd__(i,x):
+    map(i.add,init)
+  def add(i,x):
     i.ordered = False
     i.n += 1
     now  = len(i.some)
@@ -124,33 +152,40 @@ class Sample:
       i.some += [x]
     elif r() <= now/i.n:
       i.some[ int(r() * now) ]= x
-    return i
   
 class Table:
+  KINDS = {'Row':Row,'Model':Model}
+  LOVE  = ">"
+  HATE  = "<"
+  KLASS = "="
+  SKIP  = "-"
   def __init__(i):
     i.rows = []
     i.cost = 0
-    i.cols,  i.objs, i.decs = {}, [], []
+    i.cols,  i.objs, i.decs = [], [], []
     i.klass, i.gets, i.dep  = [], [], []
+  def cloned(i):
+    yield [col.txt for cols in i.cols]
+    for row in i.rows:
+      yield row.contents
   def __call__(i,row):
     if i.cols:
-      row = [i.cols[put].add( row[get] )
-             for put,get in enumerate(i.gets)]
-      i.rows += [row]
+      kind    = Table.kinds[THE.kind]
+      row     = [i.cols[put].add(row[get]) for put,get in enumerate(i.gets)])
+      i.rows += [ kind(row) ]
     else:
-      puts=-1
       for get,cell in enumerate(row):
-        if cell[0] != SKIP:
+        if cell[0] != Table.SKIP:
           i.gets += [get]
-          put    += 1
-          col     = i.cols[ len(put) ] = Thing(put, cell)
-          if   cell[0] == LOVE  : i.objs  += [(col,more)]
-          elif cell[0] == HATE  : i.objs  += [(col,less)]
-          elif cell[0] == KLASS : i.klass += [col]
-          else                  : i.decs  += [col]
+          col     = Thing( len(i.cols) , cell)    
+          i.cols += [col]
+          if   cell[0] == Table.LOVE  : i.objs  += [(col,more)]
+          elif cell[0] == Table.HATE  : i.objs  += [(col,less)]
+          elif cell[0] == Table.KLASS : i.klass += [col]
+          else                        : i.decs  += [col]
           #---------------------------------------
-          for col,_ in i.objs   : i.dep   += [col]
-          for col   in i.klass  : i.dep   += [col]
+          for col,_ in i.objs  : i.dep   += [col]
+          for col   in i.klass : i.dep   += [col]
   def distance(i,r1,r2,f=2):
     inc,n = 0, 10**-32
     for col in i.decs:
@@ -161,23 +196,6 @@ class Table:
   def label1(data,row):
     return row # usually rewritten by subclass
 
-def label(tbl, row, cost=1):
-  """Row labels are cached back into the row. So labelling
-     N times only incur a single labelling cost."""
-  def unlabelled():
-    for col in tbl.dep:
-      if row[col.pos] == UNKNOWN:
-        return True
-    return False
-  def recordLabels():
-    tbl.cost += cost
-    for col in tbl.dep:
-      col.add( row[col.pos] ) # and remember the new values   
-  if unlabelled():
-    row = tbl.label1(row,cost)
-    recordLabels()
-  return row
-
 def bdom(x, y, tbl):
   x= label(tbl, x)
   y= label(tbl, y)
@@ -185,10 +203,10 @@ def bdom(x, y, tbl):
   for col,better in tbl.objs:
     x1,y1 = x[col.pos], y[col.pos]
     if better(x1,y1) : betters += 1
-    elif x1 != y1    : return False # must be worse
+    elif x1 != y1    : return False # must be worse, go quit
   return betters > 0
 
-def cdom(x, y, tbl,ee= 2.718281828459):
+def cdom(x, y, tbl, ee= 2.718281828459):
   x= label(tbl, x)
   y= label(tbl, y)
   def w(better):
@@ -203,8 +221,8 @@ def cdom(x, y, tbl,ee= 2.718281828459):
       y1 = y[col.pos] 
       loss += exploss( w(better),x1,y1,n)
     return sum(losses) / n
-  l1 = loss(x,y)
-  l2 = loss(y,x)
+  l1= loss(x,y)
+  l2= loss(y,x)
   return l1 < l2 if abs(l1 - l2)/l1 >= THE.cdomBigger else False
 
 def sway( population, tbl, better= bdom) :
@@ -216,28 +234,31 @@ def sway( population, tbl, better= bdom) :
     if not better(west,east,tbl): cluster( easts, out )  
     if not better(east,west,tbl): cluster( wests, out )
     return out
-
-  def cosineRule(a,b,c):
-    return ( a*a + c*c - b*b )/( 2*c+ 0.0001 )
-  
+ 
   def split(items, mid,west=None,east=None):
-    west  = west or any(items)
-    east  = east or any(items)
-    while id(east)  == id(west):
+    cosine = lambda a,b,c: ( a*a + c*c - b*b )/( 2*c+ 0.0001 )
+    west   = west or any(items)
+    east   = east or any(items)
+    while east.rid  == west.rid:
       east = any(items)
     c = tbl.distance(west, east)
     xs = {}
     for item in items:
-       a = tbl.distance(item,west)
-       b = tbl.distance(item,east)
-       x = xs[ id(item) ] = cosineRule(a,b,c) # cosine rule
-       if a > c * THE.swayBigger: return split(items, mid, item, east)
-       if b > c * THE.swayBigger: return split(items, mid, west, item)   
-    items = sorted(items, key= lambda item: xs[ id(item) ])     # sorted by 'd'
+       a = tbl.distance(item, west)
+       b = tbl.distance(item, east)
+       x = xs[ item.rid ] = cosine(a,b,c) # cosine rule
+       if a > c * THE.swayBigger:
+         redo += 1
+         return split(items, mid, item, east)
+       if b > c * THE.swayBigger:
+         redo += 1
+         return split(items, mid, west, item)   
+    redo  = 0
+    items = sorted(items, key= lambda item: xs[ item.rid ]) # sorted by 'x'
     return west, east, items[:mid], items[mid:]
   # --------
   stop= max(len(population)**THE.swayCull, THE.swayStop)
-  return cluster(population, [])
+  return cluster(population, []),redo
 
 def file2table(file):
   tbl= Table()
